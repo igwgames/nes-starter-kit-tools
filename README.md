37bfe41bdf193f6624d856c47205d819e4c01537
cppchriscpp <admin@cpprograms.net>
2023-02-05 17:41:02 -0500

    Initial move of code to new repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# nes-starter-kit tools
+
+This is a small repository for the tools used by nes-starter-kit. 
+
+They are built in nodejs, and compiled into binaries for both windows and linux
+using pkg. Each tool is in a separate folder, and they are all built independently.
+
+Check the Releases tab for the latest downloads. You can extract these into the tools
+folder of your project and use them. (Though, if you use create-nes-game, this should
+be automated!)
+
+All code available under the MIT license, see LICENSE files in the subdirectories for details.
---
+++ build.sh
@@ -0,0 +1,24 @@
+# Make sure the bin directory exists
+mkdir -p bin
+
+PROJECTS="chr2img sprite_def2img tmx2c"
+WINARCHS="node16-windows-x64"
+ARCHS="node16-linux-x64 node16-linux-arm64"
+
+for PROJ in $PROJECTS; do
+    echo "Starting $PROJ"
+    cd $PROJ/src
+    npm install
+    for ARCH in $WINARCHS; do
+        echo "    Starting arch $ARCH"
+        npx pkg --public index.js --output ../../bin/$ARCH/$PROJ.exe -t $ARCH
+    done
+
+    for ARCH in $ARCHS; do
+        echo "    Starting arch $ARCH"
+        npx pkg --public index.js --output ../../bin/$ARCH/$PROJ -t $ARCH
+    done
+    echo "Finished with $PROJ"
+    cd ../..
+done
+
