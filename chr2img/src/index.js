9b15792fed25c785279f8d90b664ebc63ce78d0c
cppchriscpp <admin@cpprograms.net>
2023-02-18 00:29:03 -0500

    Bah, forgot to add optional checks to other 2 files

--- chr2img/src/index.js
+++ chr2img/src/index.js
@@ -31,7 +31,7 @@ try {
             process.exit(1);
         }
     }
-    if (chrFile.mtimeMs < outFile.mtimeMs || palFile.mtimeMs < outFile.mtimeMs) {
+    if (chrFile?.mtimeMs < outFile?.mtimeMs || palFile?.mtimeMs < outFile?.mtimeMs) {
         out(`chr and pal files unchanged, skipping execution. (Force by deleting ${process.argv[4]})`);
         process.exit(0);
     }
